754b610203eafa1320a159dd6e16e130aadfd822
ernest <ernest.ricks90@gmail.com>
2018-09-08 15:20:47 -0400

    lots of stuff

---
+++ KSU Game Jam/Assets/AIController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AIController : MonoBehaviour {
+	GameObject FPSController;
+	Transform player;               // Reference to the player's position.
+
+	NavMeshAgent nav;               // Reference to the nav mesh agent.
+	bool isSeen = false;
+
+	void Awake ()
+	{
+		// Set up the references.
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		nav = GetComponent <NavMeshAgent> ();
+	}
+
+
+	void Update ()
+	{
+		if (isSeen==true) {
+			Debug.Log ("Stop");
+
+		} else {
+			nav.SetDestination (player.position);
+		}
+
+	}
+
+}
---
+++ KSU Game Jam/Assets/SightBox.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SightBox : MonoBehaviour {
+	
+	Vector3 boxSize;
+	LayerMask mask;
+	public enum colliderState {inactive, active, colliding }
+	public Color inactiveColor;
+	public Color collidingColor;
+	public Color activeColor;
+	private colliderState state;
+
+
+	void update (){
+		Collider[] colliders = Physics.OverlapBox(transform.position, boxSize, transform.rotation, mask);
+
+		if (colliders.Length > 0) {
+			Debug.Log("We hit something");
+		}
+	
+	}
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
+		Gizmos.DrawCube(Vector3.zero, new Vector3(boxSize.x * 2, boxSize.y * 2, boxSize.z * 2));
+	}
+	private void CheckGizmoColor()
+	{
+		switch (state)
+		{
+			case colliderState.inactive:
+				Gizmos.color = inactiveColor;
+				break;
+			case colliderState.active:
+				Gizmos.color = activeColor;
+				break;
+			case colliderState.colliding:
+				Gizmos.color = collidingColor;
+				break;
+		}
+
+
+	}
+}
