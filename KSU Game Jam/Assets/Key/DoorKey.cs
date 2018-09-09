358d0e35ea5f0d1756c207327cee3866bc9a0ede
taylord109 <cisco.cat@hotmail.com>
2018-09-08 21:44:23 -0400

    Inventory Added

---
+++ KSU Game Jam/Assets/Key/DoorKey.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorKey : MonoBehaviour
+{
+
+    public string assignedDoor;
+
+    // // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Key Start");
+    }
+
+    // // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collision other)
+    {
+        Debug.Log("Colliding");
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Key Cool");
+            Inventory inventory = other.gameObject.GetComponent(typeof(Inventory)) as Inventory;
+            bool successfulAdd = inventory.addItem("Key_" + assignedDoor);
+            if (successfulAdd)
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
+
